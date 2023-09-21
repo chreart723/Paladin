d5500d1c3833ba0f426a77d8a8ff7c6127b3b50e
chreart723 <138280185+chreart723@users.noreply.github.com>
2023-09-21 13:53:44 -0400

    Update grabS3object.py, syntax errors

--- lambda/grabS3object.py
+++ lambda/grabS3object.py
@@ -4,17 +4,17 @@ import boto3
 
 def lambda_handler(event, context):
     bucket_name = "92023-typaladin-dndapp"
-    s3_obj_key = "/images/ty.jpg"
+    s3_obj_key = "images/ty.jpg"
     s3_client = boto3.client('s3', 'us-east-1')
 
-    read_from_s3 = s3_client.get_object(
-        Key=s3_obj_key
+    response = s3_client.get_object(
+        Key=s3_obj_key,
         Bucket=bucket_name
     )
     image = response['Body'].read()
     return { 
-        'headers': {"Content-Type": "image/png"},
+        'headers': {"Content-Type": "image/jpeg"},
         'statusCode': 200,
-        'body': base64.b64encode(image),
+        'body': base64.b64encode(image).decode('utf-8'),
         'isBase64Encoded': True
     }
